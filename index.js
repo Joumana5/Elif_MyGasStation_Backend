63d0c9cd4c308dd73042cc88b9310e9a6b33e0bc
jbeli1920 <157490232+jbeli1920@users.noreply.github.com>
2024-11-21 15:19:57 +0100

    email service change fel index.js w view mte3 template

--- index.js
+++ index.js
@@ -18,6 +18,25 @@ mongoose.connect('mongodb://127.0.0.1:27017/androidDB')
 app.use('/auth', authRoutes);
 app.use('/manager', managerRoutes);
 app.use('/client', clientRoutes);
+app.post('/send-email', async (req, res) => {
+    
+    const { to ,subject, text} = req.body;
+  
+    try {
+    
+        
+        await sendEmail(to, subject, text);
+
+        console.log('Email envoyé avec succès.');
+
+        return res.send("email teb3ath")
+    } catch (error) {
+        console.error('Erreur lors de l\'envoi de l\'email :', error);
+        return res.send("thama moshkla")
+    }
+   
+    
+});
 
 app.listen(3000, () => {
     console.log('Server started on port 3000!');
