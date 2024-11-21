c0d90417323368f130de5b061fe1d76bc004932f
I1AzizSaidi1I <asaidi887@gmail.com>
2024-11-21 15:51:22 +0100

    Added Client Profile
    Routes and client controller

--- Routes/ClientRoute.js
+++ Routes/ClientRoute.js
@@ -1,12 +1,18 @@
 import express from 'express';
-import { authGuard, roleGuard, redirectBasedOnRole } from '../Middleware/AuthMiddleware.js';
+import { authGuard, roleGuard } from '../Middleware/AuthMiddleware.js';
+import { getClientProfile, updateClientProfile } from '../Controller/Client.js';
 
 const router = express.Router();
-
-router.get('/dashboard', authGuard, redirectBasedOnRole);
 
 router.get('/client', authGuard, roleGuard('client'), (req, res) => {
     res.send('Welcome Client');
 });
+
+router.get('/dashboard/client', authGuard, roleGuard('client'), (req, res) => {
+    res.send('Client Dashboard');
+});
+
+router.get('/client/profile', authGuard, roleGuard('client'), getClientProfile);
+router.put('/client/profile', authGuard, roleGuard('client'), updateClientProfile);
 
 export default router;
